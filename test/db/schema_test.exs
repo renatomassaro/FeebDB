b219a4efbcb82b67c31321a147e7fde98b9b2c17
Renato Massaro <r@ena.to>
2024-10-27 10:46:45 -0300

    Add some missing tests for DB.Schema

--- test/db/schema_test.exs
+++ test/db/schema_test.exs
@@ -6,6 +6,63 @@ defmodule DB.SchemaTest do
 
   @context :test
 
+  describe "generated: __schema__/0" do
+    # `__schema__/0` contains information about fields and their types
+    schema = Friend.__schema__()
+
+    assert Map.has_key?(schema, :id)
+    assert {id_type, id_opts, id_mod} = schema.id
+    assert id_type == Feeb.DB.Type.Integer
+    assert id_opts == %{}
+    assert id_mod == nil
+  end
+
+  describe "generated: __table__/0" do
+    assert :friends == Friend.__table__()
+    assert :all_types == AllTypes.__table__()
+  end
+
+  describe "generated: __context__/0" do
+    assert :test == Friend.__context__()
+    assert :test == AllTypes.__context__()
+  end
+
+  describe "generated: __cols__/0" do
+    test "includes all non-virtual fields in order" do
+      assert [:id, :name] == Friend.__cols__()
+
+      assert [
+               :boolean,
+               :boolean_nullable,
+               :string,
+               :string_nullable,
+               :integer,
+               :integer_nullable,
+               :atom,
+               :atom_nullable,
+               :uuid,
+               :uuid_nullable,
+               :datetime_utc,
+               :datetime_utc_nullable,
+               :datetime_utc_precision_second,
+               :datetime_utc_precision_millisecond,
+               :datetime_utc_precision_microsecond,
+               :datetime_utc_precision_default,
+               :map,
+               :map_nullable,
+               :map_keys_atom,
+               :map_keys_safe_atom,
+               :map_keys_string,
+               :map_keys_default,
+               :list,
+               :list_nullable,
+               :enum,
+               :enum_nullable,
+               :enum_safe_atom
+             ] == AllTypes.__cols__()
+    end
+  end
+
   describe "generated: __virtual_cols__/0" do
     test "includes all virtual fields" do
       assert [:divorce_count] == Friend.__virtual_cols__()
